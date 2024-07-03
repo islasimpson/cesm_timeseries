e4aeabb7e3cd11820e2a905880898d0050616e17
Isla Simpson <islas@ucar.edu>
2024-07-03 16:01:13 -0600

    adding in option to specify chunk size in months

--- control/scripts/sort_timechunks.py
+++ control/scripts/sort_timechunks.py
@@ -16,14 +16,14 @@ def main():
     parser.add_argument('--tempdir', type=str, required=True, help='Location of temporary dir')
     parser.add_argument('--basepath', type=str, required=True, help='Directory containing history flies')
     parser.add_argument('--runname', type=str, required=True, help='Simulation name')
-    parser.add_argument('--ystart', type=int, required=True, help='Start year of time series generation')
-    parser.add_argument('--yend', type=int, required=True, help='End year of time series generation')
+    parser.add_argument('--datestart', type=str, required=True, help='Start year of time series generation')
+    parser.add_argument('--dateend', type=str, required=True, help='End year of time series generation')
     parser.add_argument('--chunk_size', type=int, required=True, help='Chunk size for time series chunking')
 
     args = parser.parse_args()
 
     #---???Could add other options here.  If statements depending on daily average, monthly average etc
-    sorttime_day_avg(args.tempdir,args.basepath,args.runname,args.ystart,args.yend,args.chunk_size)
+    sorttime_day_avg(args.tempdir,args.basepath,args.runname,args.datestart,args.dateend,args.chunk_size)
 
 
 def sortout_time_day_avg(dat):
@@ -51,7 +51,7 @@ def sortout_time_day_avg(dat):
         dat = dat.assign_coords(time=timenew)
     return dat
 
-def sorttime_day_avg(tempdir,basepath,runname,ystart,yend,chunk_size):
+def sorttime_day_avg(tempdir,basepath,runname,datestart,dateend,chunk_size):
     #--read in files that contain time variable
     timefiles = sorted(glob(tempdir+"*.nc"))
 
@@ -69,14 +69,27 @@ def sorttime_day_avg(tempdir,basepath,runname,ystart,yend,chunk_size):
         #---Get a list of all the times in al lthe files
         alltime.append(dat)
 
-    segment=0
-    for iyear in np.arange(ystart,yend+1,chunk_size):
 
-        #---Would need to change this if we want more flexibility beyond chunks of integer years
-        d1 = np.array(pd.date_range(start=str(iyear)+'-01-01 12:00:00', periods=1))
-        d1string = str(iyear)+'0101'
-        d2 = np.array(pd.date_range(start=str(iyear + chunk_size-1)+'-12-31 12:00:00', periods=1))
-        d2string = str(iyear+chunk_size-1)+'1231'
+    #--Convert initial date to an integer to keep track
+    idate = int(datestart)
+    d1_str = datestart
+
+    year_d1 = d1_str[0:4]
+    mon_d1 = d1_str[4:6]
+    d1 = pd.date_range(start=str(year_d1)+'-'+str(mon_d1)+'-01 12:00:00', periods=1)
+
+    #---Loop over date ranges
+    segment=0
+    while idate < int(dateend):
+
+        if (chunk_size > 0):
+            d2 = d1 + pd.DateOffset(years=chunk_size) - pd.DateOffset(days=1)
+        if (chunk_size < 0):
+            d2 = d1 + pd.DateOffset(months=np.abs(chunk_size)) - pd.DateOffset(days=1)
+
+        d1string = str(d1.year.values[0])+str(d1.month.values[0]).zfill(2)+str(d1.day.values[0]).zfill(2)
+        d2string = str(d2.year.values[0])+str(d2.month.values[0]).zfill(2)+str(d2.day.values[0]).zfill(2)
+
         fnamestring=d1string+'-'+d2string
         found_d1=False
         found_d2=False
@@ -85,7 +98,7 @@ def sorttime_day_avg(tempdir,basepath,runname,ystart,yend,chunk_size):
         #   and the indices for nco
         for ifile in np.arange(0,len(alltime),1):
             if (found_d1 == False):
-                test = np.argwhere(np.array(alltime[ifile].time) == d1)
+                test = np.argwhere(np.array(alltime[ifile].time) == d1[0])
                 if (len(test) > 0):
                     filelist=[files[ifile]]
                     sumtimes = alltime[ifile].time.size
@@ -96,7 +109,7 @@ def sorttime_day_avg(tempdir,basepath,runname,ystart,yend,chunk_size):
                 sumtimes = sumtimes + alltime[ifile].time.size
 
             if (found_d2 == False):
-                test = np.argwhere(np.array(alltime[ifile].time) == d2)
+                test = np.argwhere(np.array(alltime[ifile].time) == d2[0])
                 if (len(test) > 0):
                     id2 = test[0][0]
                     sumtimes = sumtimes - alltime[ifile].time.size + id2
@@ -111,6 +124,9 @@ def sorttime_day_avg(tempdir,basepath,runname,ystart,yend,chunk_size):
             file.write("%s\n" % str(sumtimes))
             file.write("%s\n" % fnamestring)
 
+        # --- setting dates and segment names up for the next iteration
+        d1 = d2 + pd.DateOffset(days=1)
+        idate = int(str(d1.year.values[0])+str(d1.month.values[0]).zfill(2)+str(d1.day.values[0]).zfill(2)) 
         segment = segment + 1
 
 if __name__ == "__main__":
--- control_jobs.sh
+++ control_jobs.sh
@@ -10,26 +10,29 @@ account=P93300313
 #--Run Name
 runname="f.e23.FAMIPfosi.ne0np4.NATL.ne30x8_t13.001"
 
-#--Start and End year for time series generation
-ystart=1958
-yend=2014
+#--Start and End date for time series generation (of the form YYYYMMDD)
+datestart=19580101
+dateend=19651231
 
-# Chunk size for time series output (in years)
-chunk=1
+# Chunk size for time series output
+# Chunk size in years if positive i.e., 1 gives a chunk size of 1 year
+# Chunk size in months if negative i.e., -1 gives a chunk size of 1 month
+chunk=-1
 
 #--Location of history files
-basepath="/glade/campaign/cesm/development/cvcwg/cvwg/f.e23.FAMIPfosi.ne0np4.NATL.ne30x8_t13.001_FINAL/RAW/atm/h2/"
+basepath="/glade/campaign/cesm/development/cvcwg/cvwg/f.e23.FAMIPfosi.ne0np4.NATL.ne30x8_t13.001_FINAL/RAW/atm/h3/"
 
 #--Scratch space for intermediate files (Note, any files that are in here will get deleted by the scripts)
 tempdir="/glade/derecho/scratch/islas/temp/RRAtlantic/"
 
 #--Outpath for time series files
-outpath="/glade/campaign/cesm/development/cvcwg/cvwg/f.e23.FAMIPfosi.ne0np4.NATL.ne30x8_t13.001_FINAL/timeseries/atm/day/"
+#outpath="/glade/campaign/cesm/development/cvcwg/cvwg/f.e23.FAMIPfosi.ne0np4.NATL.ne30x8_t13.001_FINAL/timeseries/atm/day/"
+outpath="/glade/derecho/scratch/islas/processed/RRAtlantic/atm/day/"
 
 #--Specify Variables
 # If specified as an empty string the it does all the vairables in the file
-VARS=""
-#VARS=( "DCQ" )
+#VARS=""
+VARS=( "PSL" )
 
 
 #-----------------END USER DEFINITIONS-------------------------------
@@ -68,14 +71,14 @@ if [ "$(ls -A $tempdir)" ] ; then
 fi
 
 #----Sort out time chunking
-qsub -A $account -v runname=$runname,basepath=$basepath,tempdir=$tempdir,ystart=$ystart,yend=$yend,chunk=$chunk run_sortout_timechunks.pbs
+qsub -A $account -v runname=$runname,basepath=$basepath,tempdir=$tempdir,datestart=$datestart,dateend=$dateend,chunk=$chunk run_sortout_timechunks.pbs
 while [[ ! -f ./control/COMPLETE ]] ; do
     echo "Sorting of the time chunks is still running..."$(date) >> ./logs/progress.txt
     sleep 60
 done
 rm ./control/COMPLETE 
 
-
+exit
 #-----First Pass
 qsub -A $account -v runname=$runname,basepath=$basepath,outpath=$outpath,vars=$VARS,firstpass=True run_tsgen.pbs 
 while [[ ! -f ./control/COMPLETE ]] ; do 
